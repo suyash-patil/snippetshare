cd3184d40fe4e7f688fe00cefba3f6c15366cecd
Suyash Patil <suyashpatil532001@gmail.com>
2023-07-14 21:26:45 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'snippetshare',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
